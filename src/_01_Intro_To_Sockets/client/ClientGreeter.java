5bea63529732d792b4b0b9c1e28a6be866f5ee9d
league <league@192.168.1.72>
2024-01-31 19:57:16 -0800

    worked on servers

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -10,20 +10,21 @@ public class ClientGreeter {
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
       String ipAddress = "localHost";
       //2. Create an integer for the server's port number
-      int portNum = 8080;
+      int portNum = 1;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
       		try {
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
+      			Socket sock = new Socket(ipAddress, portNum);
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
+      			DataOutputStream dataOutput = new DataOutputStream(sock.getOutputStream());
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
+      			dataOutput.writeUTF("Deez");
          //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
+      			DataInputStream dataInput = new DataInputStream(sock.getInputStream());
          //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
+      			System.out.println("Message from the internet " + dataInput.readUTF());
          //9. Close the client's server object
+      			sock.close();
       		}
       		catch (IOException e) {
       			e.printStackTrace();
